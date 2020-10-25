53c3cd139f5ced0462d3029c4b97a7ee671a60d5
John Melendez <john@Johns-MBP.domain>
2020-10-24 19:58:26 -0600

    finshied service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -54,3 +54,67 @@ self.addEventListener("activate", function (evt) {
             );
         })
     );
+
+    // similar to the self.skipWaiting() method, this allows us to ensure that once
+    // this service worker has been activated, it will take control of any pages
+    // needed, rather than let the old service worker run on those pages until
+    // they are refreshed
+    self.clients.claim();
+});
+
+// whenever a page controlled by this service worker tries to fetch a resource
+// (this could be an API call, or trying to load a static asset)
+self.addEventListener("fetch", function (evt) {
+    // check if the request includes "/api/" in the URL, if it does we know this is an API
+    // call, and we want to check to see if we have cached a response for this request
+    // in our DATA_CACHE.
+    if (evt.request.url.includes("/api/")) {
+        // if this request does include "/api/"" in the URL, we want the browser to
+        // respond with the following
+        evt.respondWith(
+            // open up the DATA_CACHE, and once it is open
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                // actually make the request out to the server, and once it's returned
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            // save the request in the cache for this particular URL
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        // return the response into the cache
+                        return response;
+                    })
+                    // if the browser cannot make the request (offline), or an error was returned
+                    // from the browser
+                    .catch(err => {
+                        // try to get a response from the DATA_CACHE for this request 
+                        return cache.match(evt.request);
+
+                    });
+            }).catch(err => {
+                // if an error was thrown opening the cache, or at any other point
+                console.log(err)
+            })
+        );
+
+        // we return here so that the code below this will not be run if the request
+        // contained "/api/" in the URL
+        return;
+    }
+
+    // if the request did not contain "api" in the URL, then this request was 
+    // probably for a static asset, so we're going to check the static cache
+    evt.respondWith(
+        // open up the static cache, and once that is open
+        caches.open(CACHE_NAME).then(cache => {
+            // check if the item we are trying to fetch is in the cache
+            return cache.match(evt.request).then(response => {
+                // if the cache returned a response, return that, otherwise actually
+                // fetch the resource from the server
+                return response || fetch(evt.request);
+            });
+        })
+    );
+});
